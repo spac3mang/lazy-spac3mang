340f2060813c4e926ea85739a075e394b23b9159
gil <arachnocola@gmail.com>
2026-05-02 15:10:31 -0400

    modified keybinds in outline.lua

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -1,3 +1,4 @@
+--[[
 return
     {
         "hedyhli/outline.nvim",
@@ -13,3 +14,67 @@ return
             }
         },
 }
+
+]]
+return
+    {
+        "hedyhli/outline.nvim",
+        config = function()
+            -- Example mapping to toggle outline
+            vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+                { desc = "Toggle Outline" })
+
+            require("outline").setup {
+                outline_window = {
+                    -- Where to open the split window: right/left
+                    position = 'left',
+                    auto_jump = true,
+                },
+                outline_items = {
+                    show_symbol_lineno = true
+
+                },
+
+                keymaps = {
+                    show_help = '?',
+                    close = {'<Esc>', 'q'},
+                    -- Jump to symbol under cursor.
+                    -- It can auto close the outline window when triggered, see
+                    -- 'auto_close' option above.
+                    goto_location = '<Cr>',
+                    -- Jump to symbol under cursor but keep focus on outline window.
+                    peek_location = 'o',
+                    -- Visit location in code and close outline immediately
+                    goto_and_close = '<S-Cr>',
+                    -- Change cursor position of outline window to match current location in code.
+                    -- 'Opposite' of goto/peek_location.
+                    restore_location = '<C-g>',
+                    -- Open LSP/provider-dependent symbol hover information
+                    hover_symbol = '<C-space>',
+                    -- Preview location code of the symbol under cursor
+                    toggle_preview = 'K',
+                    rename_symbol = 'r',
+                    code_actions = 'a',
+                    -- These fold actions are collapsing tree nodes, not code folding
+                    --fold = 'h',
+                    -- unfold = 'l',
+                    fold = '<Left>',
+                    unfold = '<Right>',
+
+                    fold_toggle = '<Tab>',
+                    -- Toggle folds for all nodes.
+                    -- If at least one node is folded, this action will fold all nodes.
+                    -- If all nodes are folded, this action will unfold all nodes.
+                    fold_toggle_all = '<S-Tab>',
+                    fold_all = 'W',
+                    unfold_all = 'E',
+                    fold_reset = 'R',
+                    -- Move down/up by one line and peek_location immediately.
+                    -- You can also use outline_window.auto_jump=true to do this for any
+                    -- j/k/<down>/<up>.
+                    down_and_jump = '<C-j>',
+                    up_and_jump = '<C-k>',
+                },
+            }
+        end,
+    }
